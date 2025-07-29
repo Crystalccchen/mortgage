81018f8c80032e0904f6a624a4df73502fc86732
Crystalchen <wes60145@gmail.com>
2025-07-29 23:34:05 +0800

    message

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('calcBtn').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  const amount = parseFloat(document.getElementById("amount").value) * 10000; // 萬轉元
+  const years = parseInt(document.querySelector("select#years").value);
+
+
+// 貸款期限跑迴圈
+
+})
